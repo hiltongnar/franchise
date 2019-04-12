0bb9959286916360357f9ef2b7c856d6a6cdabb0
Mike <macuser@macs-MacBook-Pro.local>
2019-04-11 20:39:50 -0400

    added footer

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # franchise
 GI Haul website
 https://hiltongnar.github.io/franchise/.
+https://script.google.com/macros/s/AKfycbxZ2uzQUrIj5h4wGXQq4IFHmgMCIX8H3UvovkzMAzHj0rtVUNuC/exec
--- script.js
+++ script.js
@@ -3,11 +3,34 @@ function getZipcode(zipcode) {
   $('#modal--zipcode').val(zipcode)
 }
 
+function formSubmit(name) {
+  var serializedData = $(document.forms[name]).serializeObject();
+  var url = 'https://script.google.com/macros/s/AKfycbxZ2uzQUrIj5h4wGXQq4IFHmgMCIX8H3UvovkzMAzHj0rtVUNuC/exec';
 
-$(document).ready(function() {
+  var jqxhr = $.ajax({
+    url,
+    method: "GET",
+    dataType: "json",
+    data: serializedData,
+    success: function(data){
+      console.log('success', data);
+    }
+  });
+}
+
+
+$(document).ready(function(){
    //event handlers;
-   $('#go-btn').click(function(){
-     var zipcode = $('#zipcode').val();
-     getZipcode(zipcode);
-   })
- });
+   $('#form-quote').submit(function(e){
+     e.preventDefault();
+
+     formSubmit(e.currentTarget.name);
+  });
+
+  $('#go-btn').click(function(){
+    var zipcode = $('#zipcode').val();
+    var email = $('[name=emai]');
+    getZipcode()
+  });
+
+});
